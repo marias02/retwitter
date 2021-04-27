e127b01447cf9da6075c615fdac4322da2401a64
Maria Arias <m.ariasmiranda02@gmail.com>
2021-04-27 10:34:12 -0400

    Removed bug 4 in while lopp in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     @user.profile_picture.attach(user_params[:profile_picture])
     if @user.username == ""
-      while User.find_by(username: @user.username) 
+      while User.find_by(username: @user.username) && @user.username != ""
         @user.username = @user.name + rand((User.all.length)..(User.all.length * 100000000)).to_s
       end
       @user.save
