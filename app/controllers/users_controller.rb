e7d60c64c7415e1738f4068e52e6a85bacd189b7
Maria Arias <m.ariasmiranda02@gmail.com>
2021-04-27 11:35:07 -0400

    Removed bugs when creating user in users controllr

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,9 +33,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     @user.profile_picture.attach(user_params[:profile_picture])
+    puts "username outside while: #{@user.username}"
     if @user.username == ""
       while User.find_by(username: @user.username)
         @user.username = @user.name + rand((User.all.length)..(User.all.length * 100000000)).to_s
+        @user.save
       end
     end
     if @user.birthdate.is_a? String
