171fd9bfea880be58013a31df0000054bd16a97e
Maria Arias <m.ariasmiranda02@gmail.com>
2021-05-13 11:23:05 -0400

    Redirecting user to home page after signup & login

--- app/javascript/functions/ApiUtils.jsx
+++ app/javascript/functions/ApiUtils.jsx
@@ -1,6 +1,3 @@
-import React from "react";
-import { Redirect } from 'react-router-dom';
-
 export const signUpUser = (user, redirect) => {
     fetch('/signup_user', {
         headers: {
@@ -11,9 +8,28 @@ export const signUpUser = (user, redirect) => {
         body: JSON.stringify(user)
     }).then(response => {
         response.json();
-        if (response.status === 204){
+        if (response.status === 204 || response.status === 200){
             redirect.push({
                 pathname: "/home", 
+                state: {cur_usr_username: user.username}
+            })
+        };
+    });
+}
+
+export const loginUser = (user, redirect) => {
+    fetch('/login_user', {
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        method: "POST",
+        body: JSON.stringify(user)
+    }).then(response => {
+        response.json();
+        if(response.status === 204 || response.status === 200){
+            redirect.push({
+                pathname: "/home",
                 state: {cur_usr_username: user.username}
             })
         };
