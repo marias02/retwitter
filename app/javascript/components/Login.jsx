fcf3514097d7544592266068e385276051d9a5b0
Maria Arias <m.ariasmiranda02@gmail.com>
2021-07-29 16:22:21 -0400

    Log In styling

--- app/javascript/components/Login.jsx
+++ app/javascript/components/Login.jsx
@@ -1,24 +1,24 @@
 import React, { Component } from "react";
 import Input from '../functions/Input';
+import ContainedButton from '../functions/ContainedButton'
 import { loginUser } from '../functions/ApiUtils';
+import { Link } from 'react-router-dom';
 
 class Login extends Component{
     constructor(props){
         super(props);
         this.state = {
             username: '',
-            password: '',
+            password_digest: ''
         };
         this.onChangeInput = this.onChangeInput.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    onChangeInput = e => {
+    onChangeInput(e){
         const name = e.target.name;
         const value = e.target.value;
-        this.setState({
-            [name]: value
-        });
+        this.setState({[name]: value});
     }
 
     async handleSubmit(e){
@@ -32,15 +32,21 @@ class Login extends Component{
 
     render(){
         return(
-            <div>
-                <form onSubmit={ this.handleSubmit }>
-                    <Input htmlFor="username" label="Username: " type="text" inputID="username" name="username" placeholder="Username" onChange={ this.onChangeInput } value={ this.state.username } />
-                    <Input htmlFor="password" label="Password: " type="password" inputID="password" name="password_digest" placeholder="Password" onChange={ this.onChangeInput } value={ this.state.password_digest } />
-                    <input type="submit" value="Login"/> 
-                </form> 
-                <div className={`message ${this.state.isError && 'error'}`}>
-                    {this.state.isSubmitting ? "Submitting..." : this.state.message}
-                </div>
+            <div id="login_container">
+                <i className="fa fa-twitter login" />
+                <h2>Log In on Retwitter</h2>
+                <Input label="Username" labelClass="form_label" 
+                inputID="username" inputClass="form_field" name="username" 
+                value={ this.state.username } onChange={ this.onChangeInput }
+                type="text" required="true" style="login" />
+                <Input label="Password" labelClass="form_label"
+                inputID="password" inputClass="form_field" name="password_digest" 
+                onChange={ this.onChangeInput } type="password" required="true"
+                value={ this.state.password_digest } style="login" />
+                <ContainedButton className="login_submit" type="primary" 
+                Text="Log In" onClick={ this.handleSubmit } />
+                <br/>
+                <Link to="/signup">Sign Up on Retwitter</Link>
             </div>
         )
     }
--- app/javascript/functions/Input.jsx
+++ app/javascript/functions/Input.jsx
@@ -1,31 +1,31 @@
 import React from "react";
 
 function Input({ label, labelClass, inputID, inputClass, name, value, onChange, 
-    type, required, readOnly }) {
+    type, required, readOnly, style }) {
     return (
         required === "true" && readOnly === "true" ? (
-            <div className="container">
+            <div className={`container ${style}`}>
                 <input type={type} id={inputID} className={inputClass} 
                 name={name} value={value} onChange={ onChange } required={ true } 
                 readOnly={ true } />
                 <span className={labelClass}>{label}</span>
             </div>
          ) : required === "true" ? (
-            <div className="container">
+            <div className={`container ${style}`}>
                 <input type={type} id={inputID} className={inputClass}
                     name={name} value={value} onChange={onChange} 
                     required={ true } />
                 <span className={labelClass}>{label}</span>
             </div>
         ) : readOnly === "true" ? (
-            <div className="container">
+            <div className={`container ${style}`}>
                 <input type={type} id={inputID} className={inputClass}
                     name={name} value={value} onChange={onChange} 
                     readOnly={ true } />
                 <span className={labelClass}>{label}</span>
             </div>
         ) : (
-            <div className="container">
+            <div className={`container ${style}`}>
                 <input type={type} id={inputID} className={inputClass}
                     name={name} value={value} onChange={onChange} />
                 <span className={labelClass}>{label}</span>
